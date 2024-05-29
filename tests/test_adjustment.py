e6023bf8fc91c478cd56b9b997a8957fcbc91ffd
huypl53 <phamlehuy53@gmail.com>
2024-05-29 08:32:27 +0700

    main: test

---
+++ tests/test_adjustment.py
@@ -0,0 +1,19 @@
+import sys
+
+import cv2
+
+from enhancing import adjust_gamma
+
+im_path = sys.argv[1]
+im = cv2.imread(im_path)
+h, w = im.shape[:2]
+n_h, n_w = int(1024 / w * h), 1024
+
+im_small = cv2.resize(im, (n_w, n_h))
+cv2.imshow("origin", im_small)
+
+enhanced_im = adjust_gamma(im, 0.4)
+enhanced_im_small = cv2.resize(enhanced_im, (n_w, n_h))
+# cv2.imshow("enhanced", enhanced_im_small)
+# cv2.waitKey(0)
+# cv2.destroyAllWindows()
