21cfb1c35ca18e2181bd35dfe8cbbcb583df91da
huypl53 <phamlehuy53@gmail.com>
2024-10-19 14:43:48 +0700

    update

--- military/make_config.py
+++ military/make_config.py
@@ -1,7 +1,7 @@
 from mmengine.config import Config
 
 base = "atss_r101_fpn_1x_coco"
-cfg = Config.fromfile(f"/kaggle/working/mmdetection/configs/atss/{base}.py")
+cfg = Config.fromfile(f"configs/atss/{base}.py")
 # ----------------------------------------------------
 width = 640
 height = 640
@@ -11,9 +11,12 @@ max_epochs = 2
 batch_size = 2
 num_classes = 1
 
-dataset_type = "CocoDataset"
-classes = ("ship",)
-data_root = "/kaggle/input/sarscope-unveiling-the-maritime-landscape/SARscope"
+dataset_type = "VOCDataset"
+classes = (
+    "radome",
+    "parabol",
+)
+data_root = "/workspace/data/RadarStation"
 # -----------------------------------------------------
 cfg.model.bbox_head.num_classes = num_classes
 # -----------------------------------------------------
@@ -23,7 +26,9 @@ cfg.test_pipeline[1]["scale"] = (width, height)
 cfg.train_dataloader.dataset.type = dataset_type
 cfg.train_dataloader.dataset.metainfo = dict(classes=classes)
 cfg.train_dataloader.dataset.data_root = data_root
-cfg.train_dataloader.dataset.ann_file = "/kaggle/input/sarscope-unveiling-the-maritime-landscape/SARscope/train/_annotations.coco.json"
+# cfg.train_dataloader.dataset.ann_file = (
+#     "/workspace/data/RadarStation/train/_annotations.coco.json"
+# )
 cfg.train_dataloader.dataset.data_prefix = dict(img="train/")
 cfg.train_dataloader.batch_size = batch_size
 cfg.train_dataloader.dataset.pipeline[2]["scale"] = (width, height)
@@ -31,25 +36,29 @@ cfg.train_dataloader.dataset.pipeline[2]["scale"] = (width, height)
 cfg.val_dataloader.dataset.type = dataset_type
 cfg.val_dataloader.dataset.metainfo = dict(classes=classes)
 cfg.val_dataloader.dataset.data_root = data_root
-cfg.val_dataloader.dataset.ann_file = "/kaggle/input/sarscope-unveiling-the-maritime-landscape/SARscope/valid/_annotations.coco.json"
+# cfg.val_dataloader.dataset.ann_file = (
+#     "/workspace/data/RadarStation/valid/_annotations.coco.json"
+# )
 cfg.val_dataloader.dataset.data_prefix = dict(img="valid/")
 cfg.val_dataloader.dataset.pipeline[1]["scale"] = (width, height)
 # -----------------------------------------------------
 cfg.test_dataloader.dataset.type = dataset_type
 cfg.test_dataloader.dataset.metainfo = dict(classes=classes)
 cfg.test_dataloader.dataset.data_root = data_root
-cfg.test_dataloader.dataset.ann_file = "/kaggle/input/sarscope-unveiling-the-maritime-landscape/SARscope/test/_annotations.coco.json"
+# cfg.test_dataloader.dataset.ann_file = (
+#     "/workspace/data/RadarStation/test/_annotations.coco.json"
+# )
 
 cfg.test_dataloader.dataset.data_prefix = dict(img="test/")
 cfg.test_dataloader.dataset.pipeline[1]["scale"] = (width, height)
 
 # ------------------------------------------------------
 cfg.val_evaluator.type = "CocoMetric"
-cfg.val_evaluator.ann_file = "/kaggle/input/sarscope-unveiling-the-maritime-landscape/SARscope/valid/_annotations.coco.json"
+# cfg.val_evaluator.ann_file = "/workspace/data/RadarStation/valid/_annotations.coco.json"
 # cfg.val_evaluator.metric=['segm']
 
 cfg.test_evaluator.type = "CocoMetric"
-cfg.test_evaluator.ann_file = "/kaggle/input/sarscope-unveiling-the-maritime-landscape/SARscope/test/_annotations.coco.json"
+# cfg.test_evaluator.ann_file = "/workspace/data/RadarStation/test/_annotations.coco.json"
 # cfg.test_evaluator.metric=['segm']
 # ------------------------------------------------------
 cfg.train_cfg.max_epochs = max_epochs
@@ -64,6 +73,9 @@ cfg.default_hooks = dict(
 
 # ------------------------------------------------------
 # !mkdir -p configs/HuBMAP
-config = f"configs/HuBMAP/custom_config_{base}_{width}_{height}.py"
+config = f"configs/atss/military_{base}_{width}_{height}.py"
 with open(config, "w") as f:
     f.write(cfg.pretty_text)
+
+# train
+# python tools/train.py configs/atss/military_atss_r101_fpn_1x_coco_640_640.py
