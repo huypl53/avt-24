e0a5168dbcdc405fb16044fef6e5744fcb5145c9
huypl53 <phamlehuy53@gmail.com>
2024-10-23 22:05:51 +0700

    update

--- military/make_config.py
+++ military/make_config.py
@@ -9,7 +9,7 @@ height = 640
 max_epochs = 2
 
 batch_size = 2
-num_classes = 1
+num_classes = 2
 
 dataset_type = "VOCDataset"
 classes = (
@@ -26,9 +26,7 @@ cfg.test_pipeline[1]["scale"] = (width, height)
 cfg.train_dataloader.dataset.type = dataset_type
 cfg.train_dataloader.dataset.metainfo = dict(classes=classes)
 cfg.train_dataloader.dataset.data_root = data_root
-# cfg.train_dataloader.dataset.ann_file = (
-#     "/workspace/data/RadarStation/train/_annotations.coco.json"
-# )
+cfg.train_dataloader.dataset.ann_file = "/workspace/data/RadarStation/train/train.txt"
 cfg.train_dataloader.dataset.data_prefix = dict(img="train/")
 cfg.train_dataloader.batch_size = batch_size
 cfg.train_dataloader.dataset.pipeline[2]["scale"] = (width, height)
@@ -36,28 +34,24 @@ cfg.train_dataloader.dataset.pipeline[2]["scale"] = (width, height)
 cfg.val_dataloader.dataset.type = dataset_type
 cfg.val_dataloader.dataset.metainfo = dict(classes=classes)
 cfg.val_dataloader.dataset.data_root = data_root
-# cfg.val_dataloader.dataset.ann_file = (
-#     "/workspace/data/RadarStation/valid/_annotations.coco.json"
-# )
+cfg.val_dataloader.dataset.ann_file = "/workspace/data/RadarStation/valid/valid.txt"
 cfg.val_dataloader.dataset.data_prefix = dict(img="valid/")
 cfg.val_dataloader.dataset.pipeline[1]["scale"] = (width, height)
 # -----------------------------------------------------
 cfg.test_dataloader.dataset.type = dataset_type
 cfg.test_dataloader.dataset.metainfo = dict(classes=classes)
 cfg.test_dataloader.dataset.data_root = data_root
-# cfg.test_dataloader.dataset.ann_file = (
-#     "/workspace/data/RadarStation/test/_annotations.coco.json"
-# )
+cfg.test_dataloader.dataset.ann_file = "/workspace/data/RadarStation/test/test.txt"
 
 cfg.test_dataloader.dataset.data_prefix = dict(img="test/")
 cfg.test_dataloader.dataset.pipeline[1]["scale"] = (width, height)
 
 # ------------------------------------------------------
-cfg.val_evaluator.type = "CocoMetric"
+cfg.val_evaluator.type = "Accuracy"
 # cfg.val_evaluator.ann_file = "/workspace/data/RadarStation/valid/_annotations.coco.json"
 # cfg.val_evaluator.metric=['segm']
 
-cfg.test_evaluator.type = "CocoMetric"
+cfg.test_evaluator.type = "Accuracy"
 # cfg.test_evaluator.ann_file = "/workspace/data/RadarStation/test/_annotations.coco.json"
 # cfg.test_evaluator.metric=['segm']
 # ------------------------------------------------------
